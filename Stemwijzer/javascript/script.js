26c8f09a94d55b51bba8545b02e9e51cff2676b4
Kawian <99052760@mydavinci.nl>
2020-04-08 15:32:53 +0200

    1e statement is te zien

--- Stemwijzer/javascript/script.js
+++ Stemwijzer/javascript/script.js
@@ -1,4 +1,21 @@
-function next() {
+
+var stellingTitle = document.getElementById("stellingTitle");
+var stellingDes = document.getElementById("stellingDes");
+
+function start() {
   document.getElementById("homePage").style.display = "none";
   document.getElementById("stellingPage").style.display = "block";
+
+  //Zet de eerste vraag klaar
+  stellingTitle.innerHTML = subjects[0].title;
+  stellingDes.innerHTML = subjects[0].statement;
+}
+
+function nextStatement(){
+      //Nieuwe stelling word geladen
+      document.getElementById('important').checked = false;
+      statementNumber++;
+      stellingTitle.innerHTML = subjects[statementNumber].title;
+      stellingDes.innerHTML = subjects[statementNumber].statement;
+      checkStatement(subjects[statementNumber].myOpinion);
 }
